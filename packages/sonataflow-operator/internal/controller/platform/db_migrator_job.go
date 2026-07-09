f28f8ada1b5371ba8e7b8e4e0f57c762e031d73d
Tibor Zimányi <tiborzimanyi@ibm.com>
2026-07-09 11:03:24 +0200

    Fix imports in go module.

--- packages/sonataflow-operator/internal/controller/platform/db_migrator_job.go
+++ packages/sonataflow-operator/internal/controller/platform/db_migrator_job.go
@@ -32,6 +32,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
